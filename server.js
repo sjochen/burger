e1e47030869b10a4cc5fa497f8a3b321310a03fb
Stephen Jochen <stephenjochen2@gmail.com>
2019-08-30 00:07:17 -0400

    made the app save new burgers to the database but still need to print to screen and then convert back to sequelize

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-
 var PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
@@ -13,6 +14,9 @@ app.set("view engine", "handlebars");
 app.get("/", function (req,res) {
     res.render("index");
 })
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
 
 app.listen(PORT, function () {
     console.log("Listening on http://localhost:" + PORT);
